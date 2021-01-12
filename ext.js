82e17be7ea6ef150a38e24267ad295514147f718
bobux-man <77365188+bobux-man@users.noreply.github.com>
2021-01-12 18:27:04 -0500

    Add files via upload

---
+++ ext.js
@@ -0,0 +1,30 @@
+class YoloBlock {
+    getInfo() {
+        return {
+            "id": "YoloBlock",
+            "name": "YoloBlock",
+            "blocks": [
+                {
+                "opcode": "string_in",
+                "blockType": "reporter",
+                "text": "string [string1] in [string2]",
+                "arguments": {
+                    "string1": {
+                        "type": "string",
+                        "defaultValue": "an",
+                    },
+                    "string2": {
+                        "type": "string",
+                        "defaultValue": "banana", //monke
+                    },
+                },
+            },
+            ],
+            "menus": {}
+        };
+    };
+    string_in({string1,string2}){
+        return (temp.match(/`string2`/g) || []).length;
+    }
+};
+Scratch.extensions.register(new YoloBlock());
